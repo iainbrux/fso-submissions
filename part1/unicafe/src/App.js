e4a8c6b22429550237e0c610da1e782519dc69ea
Iain Brookes <iain.brookes@outlook.com>
2021-05-30 17:26:49 +0100

    1.8 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,44 +1,62 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const Heading = (props) => {
-  return (
-    <h1>{props.text}</h1>
-  )
-}
+  return <h1>{props.text}</h1>;
+};
 
 const Display = (props) => {
   return (
-    <p>{props.text} {props.value}</p>
-  )
-}
+    <p>
+      {props.text} {props.value}
+    </p>
+  );
+};
 
 const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const Statistics = ({ good, bad, neutral, average }) => {
   return (
-    <button onClick={props.handleClick}>{props.text}</button>
-  )
-}
+    <>
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+      <Display text="all" value={good + neutral + bad} />
+      <Display text="average" value={average / (good + neutral + bad) || 0} />
+      <Display
+        text="positive"
+        value={((good / (good + neutral + bad)) * 100 || 0) + "%"}
+      />
+    </>
+  );
+};
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [average, setAverage] = useState(0)
+  const [average, setAverage] = useState(0);
 
   return (
     <div>
       <Heading text="give feedback" />
-      <Button handleClick={() => (setGood(good + 1), setAverage(average + 1)) } text="good" />
-      <Button handleClick={() => (setNeutral(neutral + 1), setAverage(average + 0)) } text="neutral" />
-      <Button handleClick={() => (setBad(bad + 1), setAverage(average - 1)) } text="bad" />
+      <Button
+        handleClick={() => (setGood(good + 1), setAverage(average + 1))}
+        text="good"
+      />
+      <Button
+        handleClick={() => (setNeutral(neutral + 1), setAverage(average + 0))}
+        text="neutral"
+      />
+      <Button
+        handleClick={() => (setBad(bad + 1), setAverage(average - 1))}
+        text="bad"
+      />
       <Heading text="statistics" />
-      <Display text="good" value={good}/>
-      <Display text="neutral" value={neutral} />
-      <Display text="bad" value={bad} />
-      <Display text="all" value={good + neutral + bad} />
-      <Display text="average" value={average / (good + neutral + bad) || 0} />
-      <Display text="positive" value={((good / (good + neutral + bad)) * 100 || 0) + '%'}/>
+      <Statistics good={good} neutral={neutral} bad={bad} average={average} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
