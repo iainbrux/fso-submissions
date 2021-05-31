ff068c107bee59b4cfc11a1fef89ca2a0d3f8454
iainbrux <me@iainbrux.com>
2021-05-31 22:51:18 +0100

    2.9 completion

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,22 +2,34 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterBy, setFilterBy] = useState("");
+  const [filtered, setFiltered] = useState('')
 
   const addToPhonebook = (object) => {
     if (persons.find((person) => person.name === object.name)) {
       alert(`${object.name} is already in the phonebook`);
-      return setNewName('');
+      setNewNumber("");
+      return setNewName("");
     } else {
       setPersons(persons.concat(object));
-      return setNewName('')
+      setNewNumber("");
+      return setNewName("");
     }
   };
 
-  const handleChange = (e) => {
+  const handleName = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumber = (e) => {
+    setNewNumber(e.target.value);
   };
 
   const handleSubmit = (e) => {
@@ -25,18 +37,49 @@ const App = () => {
 
     const personObject = {
       name: newName,
+      number: newNumber,
     };
 
     addToPhonebook(personObject);
   };
 
+  const handleFilter = (e) => {
+    setFilterBy(e.target.value);
+
+    const filteredNames = persons
+      .filter(
+        (person) =>
+          person.name.toLowerCase().indexOf(filterBy.toLowerCase()) >= 0
+      )
+      .map((person) => {
+        return (
+          <li key={person.name}>
+            {person.name} {person.number}
+          </li>
+        );
+      });
+
+      setFiltered(filteredNames)
+  };
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Find a contact</h2>
       <form>
         <div>
           name:
-          <input onChange={handleChange} value={newName} />
+          <input onChange={handleFilter} value={filterBy} />
+        </div>
+      </form>
+      <h2>Add a new contact</h2>
+      <form>
+        <div>
+          name:
+          <input onChange={handleName} value={newName} />
+        </div>
+        <div>
+          number:
+          <input onChange={handleNumber} value={newNumber} />
         </div>
         <div>
           <button type="submit" onClick={handleSubmit}>
@@ -45,8 +88,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
-      <div>debug: {newName}</div>
+      <div>
+        {filtered}
+      </div>
     </div>
   );
 };
